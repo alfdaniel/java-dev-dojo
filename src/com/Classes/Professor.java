64a64d7434b68a5e873ab77a7389491148e2c094
alfdaniel <daniel.alexandre22@hotmail.com>
2020-07-20 22:25:15 -0300

    Test da classe Professor

--- src/com/Classes/Professor.java
+++ src/com/Classes/Professor.java
@@ -1,9 +1,24 @@
 package com.Classes;
 
 public class Professor {
-    public String nome;
-    public int matricula;
+    private String nome;
+    public String matricula;
     public String rg;
     public String cpf;
+
+    public Professor(String nome) {
+        this.nome = nome;
+    }
+
+    public Professor() {
+
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getNome() {
+        return nome;
+    }
 
 }
--- src/com/teste/TesteProfessor.java
+++ src/com/teste/TesteProfessor.java
@@ -6,14 +6,25 @@ public class TesteProfessor {
     public static void main(String[] args) {
 
         Professor professor = new Professor();
-        professor.nome = "Karol";
-        professor.matricula = 123;
+        professor.setNome ("Karol");
+        professor.matricula = "123";
         professor.rg = "123.456-01";
         professor.cpf = "123.456.789-10";
 
-        System.out.println(professor.nome);
+        System.out.println(professor.getNome());
         System.out.println(professor.matricula);
         System.out.println(professor.rg);
         System.out.println(professor.cpf);
+
+        Professor professor2 = new Professor();
+        professor2.setNome("Daniel");
+        professor2.matricula = "1010";
+        professor2.rg = "000.000-00";
+        professor2.cpf = "111.111.111-11";
+
+        System.out.println(professor2.getNome());
+        System.out.println(professor2.matricula);
+        System.out.println(professor2.rg);
+        System.out.println(professor2.cpf);
     }
 }
